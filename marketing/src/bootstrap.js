7e635b226ed5a42ee81bcd2b7c85a6e68a3e3140
Freilin Manzano <freilin.manzano@externos.bci.cl>
2021-01-21 16:29:03 -0300

    add history navigation

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 //mount function to start up the app
-const mount = (element) => {
+const mount = (element, { onNavigate, defaultHistoy }) => {
+    const history = defaultHistoy || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+    
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         element
     );
+
+    return {
+        onParentNavigate({pathname: nextPathname}) {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 
@@ -17,7 +35,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistoy: createBrowserHistory() });
     }
 }
 
