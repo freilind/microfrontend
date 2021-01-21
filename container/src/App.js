489a01e38af7eecd9467f432821502bea9855b0f
Freilin Manzano <freilin.manzano@externos.bci.cl>
2021-01-21 10:25:09 -0300

    add header

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
     return (
-        <div>
-            <MarketingApp />
-        </div>
-        
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     )
 };
