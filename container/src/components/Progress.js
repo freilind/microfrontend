2de279a006c61ce9f41000eb6154214ee7a53234
Freilin Manzano <freilin.manzano@externos.bci.cl>
2021-01-22 12:44:50 -0300

    add theme

--- container/src/components/Progress.js
+++ container/src/components/Progress.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
-const useStyles = makeStyles(() => {
+const useStyles = makeStyles((theme) => {
     return createStyles({
         bar: {
             width: '100%',
             '& > * + *': {
-                marginTop: theme.spacing(2)
-            }
-        }
+                marginTop: theme.spacing(2),
+            },
+        },
     });
 });
 
