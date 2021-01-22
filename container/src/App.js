77225f72430e8fda22b1a8c5fbfcc79e517fc6ba
Freilin Manzano <freilin.manzano@externos.bci.cl>
2021-01-22 12:40:42 -0300

    add imports

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import Header from './components/Header';
 import Progress from './components/Progress';
